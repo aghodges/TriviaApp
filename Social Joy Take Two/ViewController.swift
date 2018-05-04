4e3fb08c3774bbf25024cd00e59565fa3dd6754a
Addison G Hodges <hodgesag@egr-maclab-21.local>
2018-05-03 23:54:08 -0400

    First commit

--- Social Joy Take Two/ViewController.swift
+++ Social Joy Take Two/ViewController.swift
@@ -1,25 +1,174 @@
 //
-//  ViewController.swift
-//  Social Joy Take Two
+//  mainMenuViewController.swift
+//  Social Joy
 //
-//  Created by Addison G Hodges on 5/3/18.
-//  Copyright © 2018 Addison G Hodges. All rights reserved.
+//  Created by Cohen, Trevor (Genworth) on 5/1/18.
+//  Copyright © 2018 Cohen, Trevor (Genworth). All rights reserved.
 //
 
 import UIKit
+import MultipeerConnectivity
 
-class ViewController: UIViewController {
+//Could add multiplayer class but for now...
 
+//var thePlayers = [MCPeerID]()
+var thePlayers = [Player]()
+
+
+class mainMenuViewController: UIViewController, MCBrowserViewControllerDelegate,MCSessionDelegate {
+    
+    @IBOutlet weak var startQuizButton: UIButton!
+    
+    var browser: MCBrowserViewController!
+    var session: MCSession!
+    var peerID: MCPeerID!
+    var assistant: MCAdvertiserAssistant!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        //var aQuiz = Quiz()
+        
+        self.peerID = MCPeerID(displayName: UIDevice.current.name)
+        self.session = MCSession(peer: peerID)
+        self.browser = MCBrowserViewController(serviceType: "quiz", session: session)
+        self.assistant = MCAdvertiserAssistant(serviceType: "quiz", discoveryInfo: nil, session: session)
+        
+        assistant.start()
+        session.delegate = self
+        browser.delegate = self
+        
+        thePlayers.append(Player(peerID: self.peerID, playerNumber: "1"))
+        
+        //Set up single player & mmultiplayer games
+        //self.singlePlayerGame = SinglePlayerGame(currentPlayer: Player(peerID: self.peerID))
+        
     }
-
+    
+    //**********************************************************
+    // required functions for MCBrowserViewControllerDelegate
+    func browserViewControllerDidFinish(_ browserViewController: MCBrowserViewController) {
+        // Called when the browser view controller is dismissed
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func browserViewControllerWasCancelled(_ browserViewController: MCBrowserViewController) {
+        // Called when the browser view controller is cancelled
+        dismiss(animated: true, completion: nil)
+    }
+    //**********************************************************
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+  
+    @IBAction func connectButtonClick(_ sender: Any) {
+       present(browser, animated: true, completion: nil)
 
-
+    }
+    
+    
+//    @IBAction func startQuiz(_ sender: Any) {
+//
+//        //If it's single player we can just start game but if its multiplayer...
+//
+//        //Send peer daata & preform segue
+//        do {
+//            let peerData = NSKeyedArchiver.archivedData(withRootObject: "StartingTheGame")
+//            try session.send(peerData, toPeers: session.connectedPeers, with: .unreliable)
+//        }
+//        catch let err {
+//            print("Data could not be sent :(")
+//        }
+//
+//        performSegue(withIdentifier: "toTheGame", sender: self)
+//
+//        //let data = NSKeyedArchiver.archivedDat(withRootObject: "START")
+//        // session.send(data, toPeers: session.connectedPeers, with: .unreliable)
+//    }
+    
+    
+    @IBAction func startQuiz(_ sender: Any) {
+        
+                //If it's single player we can just start game but if its multiplayer...
+        
+                //Send peer daata & preform segue
+                do {
+                    let peerData = NSKeyedArchiver.archivedData(withRootObject: "StartingTheGame")
+                    try session.send(peerData, toPeers: session.connectedPeers, with: .unreliable)
+                }
+                catch let err {
+                    print("Data could not be sent :(")
+                }
+        
+                performSegue(withIdentifier: "toTheGame", sender: self)
+        
+                //let data = NSKeyedArchiver.archivedDat(withRootObject: "START")
+                // session.send(data, toPeers: session.connectedPeers, with: .unreliable)
+        
+    }
+    
+    func session(_ session: MCSession, didReceive data: Data, fromPeer peerID: MCPeerID) {
+        
+        DispatchQueue.main.async(execute: {
+            
+            if let recievedString = NSKeyedUnarchiver.unarchiveObject(with: data) as? String{
+                if recievedString == "StartingTheGame"
+                {
+                    self.performSegue(withIdentifier: "toTheGame", sender: self)
+                }
+            }
+        })
+    }
+    
+    
+    func session(_ session: MCSession, didFinishReceivingResourceWithName resourceName: String, fromPeer peerID: MCPeerID, at localURL: URL?, withError error: Error?) {
+        
+    }
+    
+    
+    func session(_ session: MCSession, didStartReceivingResourceWithName resourceName: String, fromPeer peerID: MCPeerID, with progress: Progress) {
+        
+    }
+    
+    
+    func session(_ session: MCSession, didReceive stream: InputStream, withName streamName: String, fromPeer peerID: MCPeerID) {
+        
+        
+    }
+    
+    func session(_ session: MCSession, peer peerID: MCPeerID, didChange state: MCSessionState) {
+        
+        // Called when a connected peer changes state (for example, goes offline)
+        
+        switch state {
+        case MCSessionState.connected:
+            print("Connected: \(peerID.displayName)")
+            //let playerID = peerID
+            //thePlayers.append(playerID)
+            
+            if thePlayers.count == 1
+            {
+                thePlayers.append(Player(peerID: peerID, playerNumber: "2"))
+            }
+            else if thePlayers.count == 2
+            {
+                thePlayers.append(Player(peerID: peerID, playerNumber: "3"))
+            }
+            else if thePlayers.count == 3
+            {
+                thePlayers.append(Player(peerID: peerID, playerNumber:"4"))
+            }
+            
+            
+        case MCSessionState.connecting:
+            print("Connecting: \(peerID.displayName)")
+            
+        case MCSessionState.notConnected:
+            print("Not Connected: \(peerID.displayName)")
+        }
+        
+    }
+    
+    
 }
-
---
+++ Social Joy Take Two/quizScreenController.swift
@@ -0,0 +1,243 @@
+//
+//  quizScreenController.swift
+//  Social Joy
+//
+//  Created by Addison G Hodges on 5/1/18.
+//  Copyright © 2018 Cohen, Trevor (Genworth). All rights reserved.
+//
+
+
+import MultipeerConnectivity
+import UIKit
+
+class quizScreenController: UIViewController {
+    
+    
+    var browser: MCBrowserViewController!
+    var session: MCSession!
+    var peerID: MCPeerID!
+    var currentPlayer : Player!
+    
+    
+    
+//    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var timeLabel: UILabel!
+    
+    
+    var timer = Timer()
+    var timerIsRunning = false
+    var seconds = 20
+    
+    
+    
+//    @IBOutlet weak var questionLabel: UILabel!
+//
+//    @IBOutlet weak var choiceA: UIButton!
+//    @IBOutlet weak var choiceB: UIButton!
+//    @IBOutlet weak var choiceC: UIButton!
+//    @IBOutlet weak var choiceD: UIButton!
+//
+//    @IBOutlet weak var playerOneScore: UILabel!
+//    @IBOutlet weak var playerTwoScore: UILabel!
+//    @IBOutlet weak var playerThreeScore: UILabel!
+//    @IBOutlet weak var playerFourScore: UILabel!
+    
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var choiceA: UIButton!
+    @IBOutlet weak var choiceB: UIButton!
+    @IBOutlet weak var choiceC: UIButton!
+    @IBOutlet weak var choiceD: UIButton!
+    
+    @IBOutlet weak var player1Score: UILabel!
+    @IBOutlet weak var player2Score: UILabel!
+    @IBOutlet weak var player3Score: UILabel!
+    @IBOutlet weak var player4Score: UILabel!
+    
+    
+    
+    
+    
+    //var playerAnswer = String()
+    
+    
+    
+//    @IBAction func choiceASelect(_ sender: Any) {
+//        choiceA.backgroundColor = UIColor.blue
+//        choiceB.backgroundColor = UIColor.gray
+//        choiceC.backgroundColor = UIColor.gray
+//        choiceD.backgroundColor = UIColor.gray
+//
+//        currentPlayer.playerAnswer = "A"
+//
+//    }
+    
+    @IBAction func choiceASelect(_ sender: Any) {
+                choiceA.backgroundColor = UIColor.blue
+                choiceB.backgroundColor = UIColor.gray
+                choiceC.backgroundColor = UIColor.gray
+                choiceD.backgroundColor = UIColor.gray
+        
+                currentPlayer.playerAnswer = "A"
+    }
+    
+    
+    
+//    @IBAction func choiceBSelect(_ sender: Any) {
+//        choiceB.backgroundColor = UIColor.blue
+//        choiceA.backgroundColor = UIColor.gray
+//        choiceC.backgroundColor = UIColor.gray
+//        choiceD.backgroundColor = UIColor.gray
+//
+//        currentPlayer.playerAnswer = "B"
+//    }
+    
+    @IBAction func choiceBSelect(_ sender: Any) {
+        choiceB.backgroundColor = UIColor.blue
+        choiceA.backgroundColor = UIColor.gray
+        choiceC.backgroundColor = UIColor.gray
+        choiceD.backgroundColor = UIColor.gray
+        
+        currentPlayer.playerAnswer = "B"
+
+    }
+    
+//
+//    @IBAction func choiceCSelect(_ sender: Any) {
+//        choiceC.backgroundColor = UIColor.blue
+//        choiceA.backgroundColor = UIColor.gray
+//        choiceB.backgroundColor = UIColor.gray
+//        choiceD.backgroundColor = UIColor.gray
+//
+//        currentPlayer.playerAnswer = "C"
+//
+//    }
+    
+    @IBAction func choiceCSelect(_ sender: Any) {
+        choiceC.backgroundColor = UIColor.blue
+        choiceA.backgroundColor = UIColor.gray
+        choiceB.backgroundColor = UIColor.gray
+        choiceD.backgroundColor = UIColor.gray
+        
+        currentPlayer.playerAnswer = "C"
+
+    }
+    
+//
+//    @IBAction func choiceDSelect(_ sender: Any) {
+//        choiceD.backgroundColor = UIColor.blue
+//        choiceA.backgroundColor = UIColor.gray
+//        choiceB.backgroundColor = UIColor.gray
+//        choiceC.backgroundColor = UIColor.gray
+//
+//        currentPlayer.playerAnswer = "D"
+//
+//    }
+    
+    @IBAction func choiceDSelect(_ sender: Any) {
+        choiceD.backgroundColor = UIColor.blue
+        choiceA.backgroundColor = UIColor.gray
+        choiceB.backgroundColor = UIColor.gray
+        choiceC.backgroundColor = UIColor.gray
+        
+        currentPlayer.playerAnswer = "D"
+
+        
+    }
+    
+    
+    
+    
+    func startTimer() {
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: (#selector(quizScreenController.updateTheTimer)), userInfo: nil, repeats: true)
+    }
+    
+    @objc func updateTheTimer()
+    {
+        seconds = seconds - 1
+        timeLabel.text = "\(seconds)"
+        
+        if seconds == 0
+        {
+            timer.invalidate()
+            
+            endQuestion()
+        }
+    }
+    
+    func endQuestion()
+    {
+        //This is where you would change the questionLabel.text = the answer to the question. Then award points to correct players by changing the playerOneScore.text = playerOneScore (and so on).
+        //Then goToNextQuestion()
+    }
+    
+    func awardPoints()
+    {
+        //If playerAnswer == correctAnswer, chance corresponding player text label to player score + 1
+        //change timer Label to either correct or incorrect
+        
+        if currentPlayer.playerAnswer == "A"
+        {
+            questionLabel.text = "Correct!"
+            
+        }
+        else if currentPlayer.playerAnswer == "B"
+        {
+            questionLabel.text = "Incorrect"
+        }
+        
+    }
+    
+    func goToNextQuestion()
+    {
+        //Reset Question Label to current question
+        //Reset correctAnswer
+        //Reset playerAnswer
+        //Reset timer to 20 seconds
+        //Reset choices background colors all to gray
+    }
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        peerID = MCPeerID(displayName: UIDevice.current.name)
+        
+        
+        //The currentPlayer is the peerID that is equal to an object in thePlayers
+        //Throwing SIGBAT error because if theres only 2 players, there no thePlayers object data at 2 or 3 (3 or 4 players)
+        //Could fix by
+        
+        if peerID == thePlayers[0].peerID
+        {
+            currentPlayer = thePlayers[0]
+        }
+        else if thePlayers.count >= 1 && peerID == thePlayers[1].peerID
+        {
+            currentPlayer = thePlayers[1]
+        }
+        else if thePlayers.count >= 2 && peerID == thePlayers[2].peerID
+        {
+            currentPlayer = thePlayers[2]
+        }
+        else if thePlayers.count >= 3 && peerID == thePlayers[3].peerID
+        {
+            currentPlayer = thePlayers[3]
+        }
+        
+        
+        startTimer()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    
+}
+
